ce5096642d2e7f4935a7878e0d57e5c3bbed099f
Ram Bhosale <rambhosale9@gmail.com>
2023-01-18 17:57:55 +0530

    feat: make template message data

--- src/Data/TemplateMessageData.php
+++ src/Data/TemplateMessageData.php
@@ -6,20 +6,26 @@ use Rambhosale\LaravelWhatsapp\Interfaces\HasMessageData;
 
 class TemplateMessageData implements HasMessageData
 {
-    private $name;
-    private $languageCode;
+    public function __construct(private string $templateName, private string $languageCode = 'en_US') {
 
-    public function getType()
+    }
+
+    public function getType(): string
     {
         return 'template';
     }
 
-    public function toArray()
+    public function toArray(): array
     {
-        return [];
+        return [
+            'name' => $this->templateName,
+            'language' => [
+                'code' => $this->languageCode,
+            ],
+        ];
     }
 
-    public function toJson($options = 0)
+    public function toJson($options = 0) : string
     {
         return json_encode($this->toArray(), $options);
     }
